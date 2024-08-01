d7b1d1a1a59aa052db14b4eec86830b7fe9c158b
chima <chima@Ashu>
2024-08-01 11:31:19 +0530

    By Ashvini

---
+++ Zomato_Cunsumer/src/main/java/com/Zomato_Consumer/model/Orderdetails.java
@@ -0,0 +1,60 @@
+package com.Zomato_Consumer.model;
+
+import org.hibernate.annotations.GeneratorType;
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Orderdetails {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int orderId;
+private String customerName;
+private String paymentmode;
+private String customerAddress;
+private String shiftFrom;
+@ManyToOne
+private Menu orderMenu;
+public int getOrderId() {
+	return orderId;
+}
+public void setOrderId(int orderId) {
+	this.orderId = orderId;
+}
+public String getCustomerName() {
+	return customerName;
+}
+public void setCustomerName(String customerName) {
+	this.customerName = customerName;
+}
+public String getPaymentmode() {
+	return paymentmode;
+}
+public void setPaymentmode(String paymentmode) {
+	this.paymentmode = paymentmode;
+}
+public String getCustomerAddress() {
+	return customerAddress;
+}
+public void setCustomerAddress(String customerAddress) {
+	this.customerAddress = customerAddress;
+}
+public String getShiftFrom() {
+	return shiftFrom;
+}
+public void setShiftFrom(String shiftFrom) {
+	this.shiftFrom = shiftFrom;
+}
+public Menu getOrderMenu() {
+	return orderMenu;
+}
+public void setOrderMenu(Menu orderMenu) {
+	this.orderMenu = orderMenu;
+}
+}
