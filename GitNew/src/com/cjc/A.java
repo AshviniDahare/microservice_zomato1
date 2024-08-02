54f1eac293f6643ede886ea9b060c9b47f300fba
chima <chima@Ashu>
2024-08-02 11:25:27 +0530

    chenges

--- GitNew/src/com/cjc/A.java
+++ GitNew/src/com/cjc/A.java
@@ -3,10 +3,10 @@ package com.cjc;
 public class A {
 	
 public void m10(){
-	system.out.printl("m10");
+	System.out.println("m10");
 }
 	public void m11(){
-	system.out.printl("m11");
+	System.out.println("m11");
 }
 public static void main(String[] args) {
 	
